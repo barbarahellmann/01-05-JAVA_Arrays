6c220cf4b7ad230ee3ecbf0a46c89c2e09868a9c
barbarahellmann <babshellmann@web.de>
2024-03-08 10:11:55 +0100

    First steps

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // [] -> Array
+        int[] numbers = {5, 10, 4};
+
+        int sum = numbers[1] + numbers[0];
+        System.out.println(sum);
+
+    }
+}
