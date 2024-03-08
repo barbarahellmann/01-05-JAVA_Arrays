77d2f19cc661cb771be06986b24d65f7ba507d91
barbarahellmann <babshellmann@web.de>
2024-03-08 12:09:53 +0100

    working on max number

--- src/ArrayTask.java
+++ src/ArrayTask.java
@@ -10,15 +10,14 @@ public class ArrayTask {
         }
 
         System.out.println("===============");
-        System.out.println("Array with numbers: ");
+        System.out.println("1. Array with numbers: ");
 
         for (int number : numbers) {
             System.out.println(number + " ");
-
         }
 
         System.out.println("===============");
-        System.out.println("Sum of numbers in array: ");
+        System.out.println("2. Sum of numbers in array: ");
 
         int sum = 0;
         for (int number : numbers) {
@@ -28,11 +27,15 @@ public class ArrayTask {
         System.out.println(sum);
 
         System.out.println("===============");
-        System.out.println("Largest number in array: ");
+        System.out.println("3. Largest number in array: ");
 
+        int maxNumber = numbers[0];
+        for (int i = 1; i < numbers.length; i ++) {
+            if (numbers[i] > maxNumber) {
+                maxNumber = numbers[i];
+             }
 
-
-
-
+        System.out.println(maxNumber);
+        }
     }
 }
