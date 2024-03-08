98f020a4bf9317791878210b2965229250cde846
barbarahellmann <babshellmann@web.de>
2024-03-08 10:19:23 +0100

    Arrays in a long way

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,14 @@ public class Main {
         nums[0] = 5;
         nums[1] = 10;
         nums[2] = 4;
+        nums[3] = 20;
 
+    }
+
+    public static void printNumbers(int[] currywurst) {
+        System.out.println(currywurst[0]);
+        System.out.println(currywurst[1]);
+        System.out.println(currywurst[2]);
+        System.out.println(currywurst[3]);
     }
 }
